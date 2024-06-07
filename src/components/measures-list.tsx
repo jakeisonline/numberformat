85eaab6dd89714d6244b64968d50606d4286fe0c
Jake Holman <me@jakeisonline.com>
2024-06-07 22:39:55 +0100

    bring back measures

--- src/components/measures-list.tsx
+++ src/components/measures-list.tsx
@@ -40,11 +40,11 @@ export default function MeasuresList() {
   }
 
   return (
-    <ul>
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
       {MEASURE_UNITS.map((unit) => (
         <li
           key={unit}
-          className="text-lg"
+          className="text-center text-lg"
           dangerouslySetInnerHTML={{ __html: getMeasure(1000, unit) }}
         />
       ))}
