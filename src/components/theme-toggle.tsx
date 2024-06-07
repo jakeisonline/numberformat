411400c69ec9ee2c62fc7d7e7fce9d5729406916
Jake Holman <me@jakeisonline.com>
2024-06-07 22:24:52 +0100

    handle theme switch mounting better, style for interaction

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Skeleton } from "./ui/skeleton"
 
 export default function ThemeToggle() {
   const [mounted, setMounted] = useState(false)
@@ -23,28 +24,32 @@ export default function ThemeToggle() {
   }, [])
 
   if (!mounted) {
-    return null
+    return <Skeleton className="h-10 w-10 rounded-md bg-white/10" />
   }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          variant="outline"
+          size="icon"
+          className="border-black/20 dark:border-white/20"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
+        {["light", "dark", "system"].map((theme) => (
+          <DropdownMenuItem
+            className="capitalize hover:cursor-pointer hover:bg-black/10 dark:hover:bg-white/10"
+            key={theme}
+            onClick={() => setTheme(theme)}
+          >
+            {theme}
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   )
