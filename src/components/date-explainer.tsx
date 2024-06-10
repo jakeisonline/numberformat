f8a21c1e467371bd97373c81fadde0225f98d129
Jake Holman <me@jakeisonline.com>
2024-06-10 21:46:49 +0100

    better explain date formatting

--- src/components/date-explainer.tsx
+++ src/components/date-explainer.tsx
@@ -9,15 +9,19 @@ export default function DatetimeExplainer() {
   const { selectedLocale } = useSelectedLocaleContext()
 
   const currentDate = new Date()
-  const localisedDate = new Intl.DateTimeFormat(selectedLocale.value, {
-    dateStyle: "medium",
+  const localisedShortDate = new Intl.DateTimeFormat(selectedLocale.value, {
+    dateStyle: "short",
   })
-  const dateParts = localisedDate.formatToParts(currentDate)
+  const localisedLongDate = new Intl.DateTimeFormat(selectedLocale.value, {
+    dateStyle: "full",
+  })
+  const shortDateParts = localisedShortDate.formatToParts(currentDate)
+  const longDateParts = localisedLongDate.formatToParts(currentDate)
 
   return (
     <div className="text-center">
       <NumberExample>
-        {dateParts.map((part, index) => {
+        {shortDateParts.map((part, index) => {
           return (
             <PartDecorator
               key={index}
@@ -30,10 +34,38 @@ export default function DatetimeExplainer() {
           )
         })}
       </NumberExample>
-      <NumberCaption>Style: Medium</NumberCaption>
+      <NumberCaption>&nbsp;</NumberCaption>
       <p className="mt-3">
-        Some locales punctuate date parts, while others do not. Some also use a
-        different order for date parts.
+        In {selectedLocale.label} the short date is written as{" "}
+        {shortDateParts.map((part, index) => {
+          return (
+            <PartDecorator
+              key={index}
+              type={part.type}
+              matchTypes={["weekday", "day", "month", "year"]}
+              className="px-0.5"
+            >
+              {["weekday", "day", "month", "year"].indexOf(part.type) > -1
+                ? part.type
+                : part.value}
+            </PartDecorator>
+          )
+        })}
+      </p>
+      <p className="mt-3">
+        The full date should be written as{" "}
+        {longDateParts.map((part, index) => {
+          return (
+            <PartDecorator
+              key={index}
+              type={part.type}
+              matchTypes={["weekday", "day", "month", "year"]}
+              className="border-0 px-0"
+            >
+              {part.value}
+            </PartDecorator>
+          )
+        })}
       </p>
     </div>
   )
