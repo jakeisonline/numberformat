76e3c8f0a9117590cf601a89e23b6b5bee3f1686
Jake Holman <me@jakeisonline.com>
2024-06-10 22:46:21 +0100

    increase number list size

--- src/components/server/numbers-list-server.tsx
+++ src/components/server/numbers-list-server.tsx
@@ -4,6 +4,6 @@ import { generateRandomNumbersArray } from "@/lib/utils"
 import NumbersList from "@/components/numbers-list"
 
 export default async function NumbersListServer() {
-  const randomNumbers = generateRandomNumbersArray(10, -100000, 1000000)
+  const randomNumbers = generateRandomNumbersArray(16, -100000, 1000000)
   return <NumbersList randomNumbers={randomNumbers} />
 }
