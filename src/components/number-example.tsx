4144419d6edd687c270a2450dba4534ca30436f1
Jake Holman <me@jakeisonline.com>
2024-06-12 12:29:25 +0100

    remove borders on number examples

--- src/components/number-example.tsx
+++ src/components/number-example.tsx
@@ -4,7 +4,7 @@ type NumberExampleProps = {
 
 export default function NumberExample({ children }: NumberExampleProps) {
   return (
-    <div className="mb-2 inline-block rounded-md border border-black/20 bg-[#ECECE6] px-4 py-3 dark:bg-[#1B1D23]">
+    <div className="mb-2 inline-block rounded-md bg-[#ECECE6] px-4 py-3 dark:bg-[#1B1D23]">
       {children}
     </div>
   )
