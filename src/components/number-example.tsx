bd142d44ffabbb1266cd9b648dbbe524c33272bd
Jake Holman <me@jakeisonline.com>
2024-06-12 12:37:55 +0100

    make number examples a fixed height

--- src/components/number-example.tsx
+++ src/components/number-example.tsx
@@ -4,7 +4,7 @@ type NumberExampleProps = {
 
 export default function NumberExample({ children }: NumberExampleProps) {
   return (
-    <div className="mb-2 inline-block rounded-md bg-[#ECECE6] px-4 py-3 dark:bg-[#1B1D23]">
+    <div className="mb-2 inline-flex h-14 grow-0 items-center justify-center rounded-md bg-[#ECECE6] px-4 py-3 dark:bg-[#1B1D23]">
       {children}
     </div>
   )
