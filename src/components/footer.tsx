83efddd0c4a5ae33ac18f197df75bd44fd1a8e3b
Jake Holman <me@jakeisonline.com>
2024-06-14 22:12:45 +0100

    better dark/light mode for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
       <Link
         href="https://github.com/jakeisonline/number-format"
         rel="noreferrer"
-        className="group text-sm text-white/40 hover:text-white"
+        className="group text-sm text-black/55 hover:text-black dark:text-white/40 dark:hover:text-white"
       >
         👋{" "}
-        <span className="decoration-blue underline-offset-4 group-hover:text-white group-hover:underline group-hover:decoration-dotted">
+        <span className="decoration-blue underline-offset-4 group-hover:underline group-hover:decoration-dotted">
           a tiny app by jake
         </span>
       </Link>
