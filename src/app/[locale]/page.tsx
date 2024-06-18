ce3c56f890822d6b9925ac880d698498fcd6faae
Jake Holman <me@jakeisonline.com>
2024-06-18 12:47:51 +0100

    dynamic descriptions for locale pages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -18,10 +18,26 @@ import { RelativeDateList } from "@/components/relative-date-list"
 import { RelativeTimeList } from "@/components/relative-time-list"
 import SelectedLanguageContextProvider from "@/contexts/selected-locale-context-provider"
 import { getHeadersLocale } from "@/lib/server-utils"
+import type { ResolvingMetadata, Metadata } from "next"
+import { getLocaleByValue } from "@/lib/utils"
 
 type LocalePageProps = {
   params: {
     locale: string
+  }
+}
+
+export async function generateMetadata(
+  { params }: LocalePageProps,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const locale = getLocaleByValue(params.locale)
+  return {
+    description: `See the special formatting rules for numbers in ${locale.label} (${locale.value}). This tool helps you look up the right format for every locale.`,
+    openGraph: {
+      description: `See the special formatting rules for numbers in ${locale.label} (${locale.value}). This tool helps you look up the right format for every locale.`,
+      url: `https://www.numberformat.app/${locale.value.toLocaleLowerCase()}/`,
+    },
   }
 }
 
