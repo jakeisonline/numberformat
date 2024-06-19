87ab2a0779f37d0a66422c31e7acf33c91362264
Jake Holman <me@jakeisonline.com>
2024-06-19 22:59:43 +0100

    fix odd line appearing on sticky locale selector

--- src/components/locale-selection.tsx
+++ src/components/locale-selection.tsx
@@ -34,10 +34,10 @@ export default function LocaleSelection() {
 function StickyLocaleWrapper({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="bg-page sticky top-0 z-20 mx-auto h-[112px] py-3 text-center">
+      <div className="bg-page sticky top-[-1px] z-20 mx-auto h-[112px] py-3 text-center">
         {children}
       </div>
-      <div className="sticky top-[112px] h-2 w-full bg-gradient-to-b from-neutral-200 dark:from-neutral-900" />
+      <div className="sticky top-[111px] h-2 w-full bg-gradient-to-b from-neutral-200 dark:from-neutral-900" />
       <div className="bg-page sticky top-0 z-10 -mt-2 h-2 w-full" />
     </>
   )
