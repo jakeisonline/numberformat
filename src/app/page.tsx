2186dea22bd21ef3d80cb72df456ef71608e2b98
Jake Holman <me@jakeisonline.com>
2024-06-19 22:57:28 +0100

    refactor page templates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,14 @@
-import CurrenciesExplainer from "@/components/currencies-explainer"
-import DateExplainer from "@/components/date-explainer"
-import LocaleSelection from "@/components/locale-selection"
-import NumberContainer from "@/components/number-container"
-import NumberIcon from "@/components/number-icon"
-import NumbersExplainer from "@/components/numbers-explainer"
-import NumberHeading from "@/components/number-heading"
-import NumbersWrapper from "@/components/numbers-wrapper"
-import CurrenciesListServer from "@/components/server/currencies-list-server"
-import NumbersListServer from "@/components/server/numbers-list-server"
-import ThemeToggle from "@/components/theme-toggle"
-import TimeExplainer from "@/components/time-explainer"
-import MeasuresListServer from "@/components/server/measures-list-server"
-import { MeasuresFullToggle } from "@/components/measures-full-toggle"
-import FullMeasuresContextProvider from "@/contexts/full-measures-context-provider"
-import { RelativeDateList } from "@/components/relative-date-list"
-import { RelativeTimeList } from "@/components/relative-time-list"
 import SelectedLanguageContextProvider from "@/contexts/selected-locale-context-provider"
 import { getHeadersLocale } from "@/lib/server-utils"
 import PlausibleProvider from "next-plausible"
+import IndexTemplate from "@/components/templates/index-template"
 
 export default function Home() {
   const browserLocale = getHeadersLocale()
   return (
     <>
       <SelectedLanguageContextProvider browserLocale={browserLocale}>
-        <main className="mx-auto max-w-6xl px-2 py-2 md:px-6 md:py-4 lg:py-6">
-          <header className="flex justify-end">
-            <ThemeToggle />
-          </header>
-          <div className="relative mx-auto flex w-full flex-auto justify-center md:mb-5 lg:mt-8">
-            <section className="flex w-screen flex-col md:w-10/12">
-              <h1 className="font-display inline text-center text-3xl font-bold tracking-tight md:text-5xl lg:text-6xl">
-                Every <span className="text-orange">number format</span>,
-                <br /> for every <span className="text-blue">locale</span>
-              </h1>
-            </section>
-          </div>
-          <LocaleSelection />
-          <NumbersWrapper>
-            <NumberContainer>
-              <NumberHeading>
-                <NumberIcon iconName="hashtag" />
-                Numbers
-              </NumberHeading>
-              <NumbersExplainer />
-              <NumbersListServer />
-            </NumberContainer>
-            <NumberContainer>
-              <NumberHeading>
-                <NumberIcon iconName="calendar" />
-                Dates
-              </NumberHeading>
-              <DateExplainer />
-              <RelativeDateList />
-            </NumberContainer>
-            <NumberContainer>
-              <NumberHeading>
-                <NumberIcon iconName="clock" />
-                Times
-              </NumberHeading>
-              <TimeExplainer />
-              <RelativeTimeList />
-            </NumberContainer>
-            <NumberContainer>
-              <NumberHeading>
-                <NumberIcon iconName="banknotes" />
-                Currencies
-              </NumberHeading>
-              <CurrenciesExplainer />
-              <CurrenciesListServer />
-            </NumberContainer>
-            <NumberContainer className="md:col-span-2">
-              <FullMeasuresContextProvider>
-                <NumberHeading>
-                  <NumberIcon iconName="cube-transparent" />
-                  Measures
-                  <MeasuresFullToggle />
-                </NumberHeading>
-                <MeasuresListServer />
-              </FullMeasuresContextProvider>
-            </NumberContainer>
-          </NumbersWrapper>
-        </main>
+        <IndexTemplate />
       </SelectedLanguageContextProvider>
       <PlausibleProvider
         domain="numberformat.app"
---
+++ src/components/templates/index-template.tsx
@@ -0,0 +1,80 @@
+import FullMeasuresContextProvider from "@/contexts/full-measures-context-provider"
+import CurrenciesExplainer from "../currencies-explainer"
+import NumbersExplainer from "../numbers-explainer"
+import DateExplainer from "../date-explainer"
+import LocaleSelection from "../locale-selection"
+import NumberContainer from "../number-container"
+import NumberHeading from "../number-heading"
+import NumberIcon from "../number-icon"
+import NumbersWrapper from "../numbers-wrapper"
+import { RelativeDateList } from "../relative-date-list"
+import { RelativeTimeList } from "../relative-time-list"
+import CurrenciesListServer from "../server/currencies-list-server"
+import NumbersListServer from "../server/numbers-list-server"
+import ThemeToggle from "../theme-toggle"
+import TimeExplainer from "../time-explainer"
+import { MeasuresFullToggle } from "../measures-full-toggle"
+import MeasuresListServer from "../server/measures-list-server"
+
+export default function IndexTemplate() {
+  return (
+    <main className="mx-auto max-w-6xl px-2 py-2 md:px-6 md:py-4 lg:py-6">
+      <header className="flex justify-end">
+        <ThemeToggle />
+      </header>
+      <div className="relative mx-auto flex w-full flex-auto justify-center md:mb-5 lg:mt-8">
+        <section className="flex w-screen flex-col md:w-10/12">
+          <h1 className="font-display inline text-center text-3xl font-bold tracking-tight md:text-5xl lg:text-6xl">
+            Every <span className="text-orange">number format</span>,
+            <br /> for every <span className="text-blue">locale</span>
+          </h1>
+        </section>
+      </div>
+      <LocaleSelection />
+      <NumbersWrapper>
+        <NumberContainer>
+          <NumberHeading>
+            <NumberIcon iconName="hashtag" />
+            Numbers
+          </NumberHeading>
+          <NumbersExplainer />
+          <NumbersListServer />
+        </NumberContainer>
+        <NumberContainer>
+          <NumberHeading>
+            <NumberIcon iconName="calendar" />
+            Dates
+          </NumberHeading>
+          <DateExplainer />
+          <RelativeDateList />
+        </NumberContainer>
+        <NumberContainer>
+          <NumberHeading>
+            <NumberIcon iconName="clock" />
+            Times
+          </NumberHeading>
+          <TimeExplainer />
+          <RelativeTimeList />
+        </NumberContainer>
+        <NumberContainer>
+          <NumberHeading>
+            <NumberIcon iconName="banknotes" />
+            Currencies
+          </NumberHeading>
+          <CurrenciesExplainer />
+          <CurrenciesListServer />
+        </NumberContainer>
+        <NumberContainer className="md:col-span-2">
+          <FullMeasuresContextProvider>
+            <NumberHeading>
+              <NumberIcon iconName="cube-transparent" />
+              Measures
+              <MeasuresFullToggle />
+            </NumberHeading>
+            <MeasuresListServer />
+          </FullMeasuresContextProvider>
+        </NumberContainer>
+      </NumbersWrapper>
+    </main>
+  )
+}
