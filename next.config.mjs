eab7c1efa320cf78b3fae056f492442bccdf12a5
Jake Holman <me@jakeisonline.com>
2024-06-19 12:26:17 +0100

    proxy plausible

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+import { withPlausibleProxy } from "next-plausible"
 const nextConfig = {}
 
-export default nextConfig
+export default withPlausibleProxy()(nextConfig)
