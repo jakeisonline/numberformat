7e18a2457d483ff22a286197f79c5ee56832fc2b
Jake Holman <me@jakeisonline.com>
2024-06-20 22:09:36 +0100

    sitemap urls should not have trailing slashes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   // Add static pages
   sitemap.push({
-    url: "https://www.numberformat.app/",
+    url: "https://www.numberformat.app",
     lastModified: new Date(),
     changeFrequency: "monthly",
     priority: 1,
@@ -16,7 +16,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   LOCALES.forEach((locale) => {
     const lowerCaseLocale = locale.value.toLowerCase()
     sitemap.push({
-      url: `https://www.numberformat.app/${lowerCaseLocale}/`,
+      url: `https://www.numberformat.app/${lowerCaseLocale}`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
