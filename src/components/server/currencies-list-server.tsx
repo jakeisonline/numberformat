501c7691e06d3d448a4491e4b0438dc410d043e4
Jake Holman <me@jakeisonline.com>
2024-06-26 22:58:09 +0100

    reduce the range of numbers for currencies

--- src/components/server/currencies-list-server.tsx
+++ src/components/server/currencies-list-server.tsx
@@ -4,6 +4,6 @@ import { generateRandomNumbersArray } from "@/lib/utils"
 import CurrenciesList from "@/components/currencies-list"
 
 export default async function CurrenciesListServer() {
-  const randomNumbers = generateRandomNumbersArray(10, -100000, 1000000)
+  const randomNumbers = generateRandomNumbersArray(10, 100, 100000)
   return <CurrenciesList randomNumbers={randomNumbers} />
 }
