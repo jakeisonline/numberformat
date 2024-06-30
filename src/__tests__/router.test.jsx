c9079d087a53dbc1631b1f47080ddf7dbc813048
Jake Holman <me@jakeisonline.com>
2024-06-30 20:59:28 +0100

    correctly look for NEXT_NOT_FOUND

--- src/__tests__/router.test.jsx
+++ src/__tests__/router.test.jsx
@@ -1,11 +1,10 @@
 import LocalePage from "@/app/[locale]/page"
 import "@testing-library/jest-dom"
-import { render } from "@testing-library/react"
 
 describe("locale page routing", () => {
-  it("should render NEXT_NOT_FOUND when invalid locale is provided", () => {
+  it("should throw NEXT_NOT_FOUND when invalid locale is provided", () => {
     expect(() => {
-      render(<LocalePage params={{ locale: "does-not-exist" }} />)
+      LocalePage({ params: { locale: "invalid" } })
     }).toThrow("NEXT_NOT_FOUND")
   })
 })
