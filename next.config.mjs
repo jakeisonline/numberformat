e6cf82e9580444bcaf1e2c18068168a871dd3496
Jake Holman <me@jakeisonline.com>
2024-07-01 22:33:35 +0100

    allow bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 import { withPlausibleProxy } from "next-plausible"
-const nextConfig = {}
+import withBundleAnalyzer from "@next/bundle-analyzer"
+import { Config } from "next-recompose-plugins"
 
-export default withPlausibleProxy()(nextConfig)
+const nextConfig = new Config({})
+  .applyPlugin((phase, args, nextConfig) => {
+    return withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })(
+      nextConfig,
+    )
+  }, "@next/bundle-analyzer")
+  .applyPlugin((phase, args, nextConfig) => {
+    return withPlausibleProxy()(nextConfig)
+  }, "next-plausible")
+  .build()
+
+export default nextConfig
