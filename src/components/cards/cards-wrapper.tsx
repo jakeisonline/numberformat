7fdd3835f12fac8807bfc0452bd52f6c18babd00
Jake Holman <me@jakeisonline.com>
2024-07-03 21:53:41 +0100

    renamed NumbersWrapper to CardsWrapper

--- src/components/cards/cards-wrapper.tsx
+++ src/components/cards/cards-wrapper.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 
-type NumbersWrapperProps = {
+export default function CardsWrapper({
+  children,
+}: {
   children: React.ReactNode
-}
-
-export default function NumbersWrapper({ children }: NumbersWrapperProps) {
+}) {
   return (
     <div className="grid gap-6 md:mt-4 md:grid-cols-2 lg:grid-cols-3">
       {children}
