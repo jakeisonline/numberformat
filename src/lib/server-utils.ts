3d810755fc3ca53125463a65b083e710a4a9484f
Jake Holman <me@jakeisonline.com>
2024-07-03 22:01:04 +0100

    log when no accept-language header has been found

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -5,7 +5,10 @@ import { headers } from "next/headers"
 export function getHeadersLocale() {
   const headersLocaleString = headers().get("Accept-Language")
 
-  if (!headersLocaleString) return "en"
+  if (!headersLocaleString) {
+    console.log(`No Accept-Language header found`)
+    return "en"
+  }
 
   return headersLocaleString.split(",")[0].split(";")[0]
 }
