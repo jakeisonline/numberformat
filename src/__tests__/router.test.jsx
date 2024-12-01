6e4ccf27bdafa21c76af440721f0af561c253f4b
Jake Holman <me@jakeisonline.com>
2024-12-01 21:19:37 +0000

    fix failing route test with correct async test

--- src/__tests__/router.test.jsx
+++ src/__tests__/router.test.jsx
@@ -2,9 +2,9 @@ import LocalePage from "@/app/[locale]/page"
 import "@testing-library/jest-dom"
 
 describe("locale page routing", () => {
-  it("should throw NEXT_NOT_FOUND when invalid locale is provided", () => {
-    expect(() => {
-      LocalePage({ params: { locale: "invalid" } })
-    }).toThrow("NEXT_NOT_FOUND")
+  it("should throw NEXT_NOT_FOUND when invalid locale is provided", async () => {
+    await expect(LocalePage({ params: { locale: "invalid" } })).rejects.toThrow(
+      "NEXT_NOT_FOUND",
+    )
   })
 })
